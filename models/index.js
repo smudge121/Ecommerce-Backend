d5eb57f61a7837f00abc0f14d63dca736a6732e7
Jishii <33364545+smudge121@users.noreply.github.com>
2022-12-19 19:11:08 -0800

    Added model code and joins

--- models/index.js
+++ models/index.js
@@ -5,12 +5,36 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: {
+    name: 'category_id',
+  }
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: {
+    name: 'category_id'
+  }
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
